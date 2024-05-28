c7f3e95d7da8a6da39c5055f33eb4049faedc90a
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-05-28 12:23:16 +0530

    define getComputerChoice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let x = Math.floor(Math.random()*3)
+    if (x==0) {
+        return "rock"
+    }
+    if (x==1) {
+        return "paper"
+    }
+    if (x==2) {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice())
